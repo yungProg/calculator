a1153130f938fd0c70263121d23f9ca931a05472
yungProg <gboakye007@outlook.com>
2024-03-31 22:36:27 +0000

    all operations functions. read readMe for impending bug

--- script.js
+++ script.js
@@ -1,97 +1,105 @@
-const display = document.querySelector(".display")
-const clear = document.getElementById("clear")
-const plusOrNegative= document.getElementById("plusOrNegative")
-const percentage = document.getElementById("percentage")
-const digits = document.querySelectorAll(".number")
-const decimalPoint = document.getElementById("decimalPoint")
+// lists of elements
+const display = document.getElementById("display")
+const number = document.querySelectorAll(".number")
 const operators = document.querySelectorAll(".operator")
 const equal = document.getElementById("equal")
+const clear = document.getElementById("clear")
+const negate = document.getElementById("negate")
+const percentage = document.getElementById("percentage")
+const decimalPoint = document.getElementById("decimalPoint")
 
-let firstNum = 0
-let operator = ""
+// global variables
+let firstNum = ""
 let secondNum = ""
-let result = ""
+let operator = ""
+let isEqualSignClicked = false
 
-function val(param) {
-    if (display.value === "0"  || display.value.length == 0) {
-        display.value = param.innerText
-    } else {
-        display.value += param.innerText
-    }
-    if (operator !== "") {
-        display.value = ""
-        secondNum += param.innerText
-        display.value += secondNum
-    } 
-    display.value > 0 ? clear.innerText = "C" : clear.innerText = "AC"
-}
-
-for (let i = 0; i < digits.length; i++) {
-    const element = digits[i];
-    element.addEventListener("click", () => {val(element)})
-}
-
-clear.addEventListener("click", () => {
-    display.value = "0"
-    clear.innerText = "AC"
-    firstNum = 0
-    operator = ""
-    secondNum = ""
-    result = ""
-})
-
-plusOrNegative.addEventListener("click", () => {
-    display.value *= -1
-    console.log(typeof display.value)
-})
-
-percentage.addEventListener("click", () => {
-    display.value = Number(display.value)/100
-    console.log(typeof display.value);
-})
-
-decimalPoint.addEventListener("click", () => {
-    let displayArr = display.value.split("");
-    let secondNumArr = secondNum.split("")
-    if (display.value) {
-        displayArr.some(item => item == ".") ? "" : display.value += decimalPoint.innerText
-    }
-    if (secondNum) {
-        secondNumArr.some(item => item == ".") ? "" : secondNum += decimalPoint.innerText
-    }
-})
-
-//console.log(digits);
-
-// code for operations
-
-
-function cal(el) {
-    operator = el.value
-    if (operator === "") {
-        firstNum = display.value
-        //operator = el.value
-    } else {
-        result = Function(`return ${firstNum} ${operator} ${secondNum}`)()
-        console.log(result);
-        display.value = result
-        firstNum = result
-        operator = el.value
+// display digits
+function displayDigit(digit) {
+    if (isEqualSignClicked) {
+        firstNum = ""
         secondNum = ""
+        display.value = firstNum
+        operator = ""
+        isEqualSignClicked = false
     }
-    console.log(firstNum, operator, secondNum)
+    if (operator === "") {
+        firstNum += digit
+        display.value = firstNum
+    }
+    if (operator) {
+        secondNum += digit
+        display.value = secondNum
+    }    
+    display.value != "0" ? clear.textContent = "C" : clear.textContent = "AC"
+    console.log("first Number" ,firstNum);
+    console.log("second Number" ,secondNum);
+    console.log(isEqualSignClicked);
+}
+
+for (let i = 0; i < number.length; i++) {
+    number[i].addEventListener("click", () => {displayDigit(number[i].textContent)})
+}
+
+// operator capture
+function operation(mathOperator) {
+    if (secondNum) {
+        if (!isEqualSignClicked) {
+            firstNum = Function(`return ${firstNum} ${operator} ${secondNum}`)()
+            firstNum == "Infinity" ? firstNum = "Error" : ""
+        }
+        isEqualSignClicked = false
+        display.value = firstNum
+        secondNum = ""
+        console.log(firstNum)
+    }
+    operator = mathOperator
+    console.log(operator);
 }
 
 for (let i = 0; i < operators.length; i++) {
-    const element = operators[i];
-    element.addEventListener("click", () => {cal(element)})
+    operators[i].addEventListener("click", () => {operation(operators[i].value)})
 }
 
-equal.addEventListener("click", () => {
-    if (!secondNum) {
-        result = firstNum
-    } else {
-        result = Function(`return ${firstNum} ${operator} ${secondNum}`)()
-    }
-    display.value = result
+// second number capture
+
+
+// calculate with equal 
+function equalSign() {
+    firstNum = Function(`return ${firstNum} ${operator} ${secondNum}`)()
+    if (firstNum == "Infinity") {
+        display.value = "Error"
+    } else display.value = firstNum
+    isEqualSignClicked = true
+    console.log(isEqualSignClicked)
+}
+
+equal.addEventListener("click", equalSign)
+
+// clear second number
+
+// clear everything
+clear.addEventListener("click", () => {
+    clear.textContent = "AC"
+    firstNum = ""
+    secondNum = ""
+    operator = ""
+    display.value = 0
+    isEqualSignClicked = false
+})
+
+negate.addEventListener("click", () => {
+    firstNum == display.value ? firstNum *= -1 : ""
+    display.value = firstNum
+})
+
+percentage.addEventListener("click", () => {
+    firstNum == display.value ? firstNum /= 100 : ""
+    display.value = firstNum
+})
+
+decimalPoint.addEventListener("click", () => {
+    let numArr = firstNum.split("")
+    numArr.some(item => item == ".") ? "" : firstNum += "."
+    display.value = firstNum
 })
